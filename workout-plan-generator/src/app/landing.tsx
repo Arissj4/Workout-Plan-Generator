f9de1dbfdb3fe21fdb54e550f812efcc77b54602
Soheil Jamshidi <eris1376@gmail.com>
2026-05-04 16:39:32 +0200

    WPG [ADD] Updated the landing page

--- workout-plan-generator/src/app/landing.tsx
+++ workout-plan-generator/src/app/landing.tsx
@@ -20,9 +20,23 @@ export default function Landing(){
           <p className="wrapper-left--sub text-[15px] text-(--wpg-muted-text-color) max-w-95 mb-10 leading-[1.7]">
             Answer a few questions. Get a personalized weekly workout plan built for your body, your goals, and your schedule.
           </p>
+
+          <div className="flex gap-3">
+            <button
+              className="bg-(--wpg-main-text-color) px-8 py-3 text-black text-[14px] font-medium cursor-pointer"
+            >
+              Generate Free
+            </button>
+
+            <button
+              className="bg-transparent px-8 py-3 text-(--wpg-muted-text-color) text-[14px] font-medium border border-[#2e2e2e] cursor-pointer"
+            >
+              View Saved Plan
+            </button>
+          </div>
         </div>
 
-        <div className="wrapper wrapper-right bg-[#1a1a1a]">
+        <div className="wrapper wrapper-right w-full max-w-[50%] h-auto bg-[#1a1a1a] border-l border-[#2e2e2e]">
 
         </div>
       </div>
