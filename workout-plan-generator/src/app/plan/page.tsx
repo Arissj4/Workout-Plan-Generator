1c2a8d65f90435b6ae8ba8c198513c090f24de17
Soheil Jamshidi <eris1376@gmail.com>
2026-05-11 17:53:33 +0200

    WPG [ADD] Implemented the first version of export to PDF for plans

--- workout-plan-generator/src/app/plan/page.tsx
+++ workout-plan-generator/src/app/plan/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import { useRouter } from "next/navigation";
+import { jsPDF } from "jspdf";
 
 type Exercise = {
   name: string,
@@ -27,6 +28,8 @@ type Plan = {
 };
 
 export default function Plan(){
+
+  const [isDownloading, setIsDownloading] = useState<Boolean>(false);
 
   const [plan, setPlan] = useState<Plan | null>(null);
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -55,94 +58,156 @@ export default function Plan(){
     console.log(plan);
   }, [plan]);
 
+  const handleExportAsPDF = () => {
+    try{
+      setIsDownloading(true);
+      const doc = new jsPDF();
+
+      const pageWidth = doc.internal.pageSize.getWidth();
+      const pageHeight = doc.internal.pageSize.getHeight();
+
+      // Black page background
+      doc.setFillColor(0, 0, 0);
+      doc.rect(0, 0, pageWidth, pageHeight, "F");
+
+      doc.setFontSize(24);
+      doc.setFont("helvetica", "bold");
+      doc.setTextColor("E8FF47");
+      doc.text("WPG", 20, 20);
+
+      doc.setTextColor("white");
+      doc.setFont("helvetica", "normal");
+      doc.setFontSize(16);
+      doc.text("Workout List", 20, 40);
+
+      doc.setFontSize(12);
+
+      let y = 60;
+
+      plan?.days.forEach((dayPlan, index) => {
+        if (!dayPlan.isRest) {
+          doc.text(
+            `${dayPlan.day} - ${dayPlan.focus}`,
+            20,
+            y
+        );
+
+        y += 10;
+          dayPlan.exercises.forEach((exercise, exIndex) => {
+            doc.text(`  ${exIndex + 1}. ${exercise.name} - ${exercise.sets}x${exercise.reps}`, 20, y);
+            y += 7;
+          });
+          y += 5; // Add some space after each day's exercises
+        }
+      });
+
+      doc.save("workouts.pdf");
+    } catch (error){
+      console.log(error);
+    } finally {
+      setIsDownloading(false);
+    }
+  }
+
   if (!plan) return <div className="flex flex-1 p-12 text-white items-center justify-center text-4xl animate-pulse">Loading plan...</div>;
 
   return (
-    <div className="flex flex-col p-12 max-w-6xl mx-auto">
-      <div className="flex">
-        <div className="flex max-w-[70%]">
-          <h1 className="text-[56px] uppercase tracking-[1px] leading-none mb-4">
-            {plan.title}
-          </h1>
+
+    <>
+      {isDownloading ?
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50 text-white text-3xl animate-pulse flex-col gap-4 text-center ">
+          <div className="loader"></div>
+          Preparing your plan for download...
+        </div>
+      : null}
+
+      <div className="flex flex-col p-12 max-w-6xl mx-auto">
+        <div className="flex">
+          <div className="flex max-w-[70%]">
+            <h1 className="text-[56px] uppercase tracking-[1px] leading-none mb-4">
+              {plan.title}
+            </h1>
+          </div>
+
+          <div className="flex flex-1 justify-end gap-2">
+            <button
+              className="flex justify-center items-center max-h-11.75 gap-1 bg-transparent px-8 py-3 text-(--wpg-muted-text-color) text-[14px] font-medium border border-[#2e2e2e] cursor-pointer"
+              onClick={() => handleExportAsPDF()}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+              </svg>
+              Export as PDF
+            </button>
+
+            <button
+              className="max-h-11.75 bg-(--wpg-disabled-color) px-8 py-3 text-black text-[14px] font-medium "
+              disabled={true}
+            >
+              Save Plan
+            </button>
+          </div>
         </div>
 
-        <div className="flex flex-1 justify-end gap-2">
-          <button
-            className="flex justify-center items-center max-h-11.75 gap-1 bg-transparent px-8 py-3 text-(--wpg-muted-text-color) text-[14px] font-medium border border-[#2e2e2e] cursor-pointer"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3.5">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
-            </svg>
-            Export
-          </button>
+        <div className="flex gap-5">
+          <div className="tag">
+            {plan.level}
+          </div>
 
-          <button
-            className="max-h-11.75 bg-(--wpg-disabled-color) px-8 py-3 text-black text-[14px] font-medium "
-            disabled={true}
-          >
-            Save Plan
-          </button>
-        </div>
-      </div>
+          <div className="tag">
+            {`${plan.days.length} ${plan.days.length === 1 ? "day" : "days"} / week`}
+          </div>
 
-      <div className="flex gap-5">
-        <div className="tag">
-          {plan.level}
+          <div className="tag">
+            {`${plan.time} min`}
+          </div>
+
+          <div className="tag">
+            {plan.equipment.join(" + ")}
+          </div>
         </div>
 
-        <div className="tag">
-          {`${plan.days.length} ${plan.days.length === 1 ? "day" : "days"} / week`}
-        </div>
-
-        <div className="tag">
-          {`${plan.time} min`}
-        </div>
-
-        <div className="tag">
-          {plan.equipment.join(" + ")}
-        </div>
-      </div>
-
-      <div className="grid grid-cols-3 gap-4 mt-10">
-        {days.map((day, index) => (
-          <div
-            key={index}
-            className={`border p-5 ${
-              plan.days.find(d => day === d.day)
-                ? "border-[#2e2e2e] bg-[#1a1a1a]"
-                : "border-dashed border-[#2e2e2e] flex justify-center items-center "
-            }`}
-          >
-            {plan.days.find(d => day === d.day) ?
-            (
-              <div className="text-[22px] uppercase tracking-[1px] text-(--wpg-accent-color) mb-3">
-                {day} — {plan.days.find(d => day === d.day)?.focus}
-              </div>
-            )
-            : null
-            }
-
-            {plan.days.find(d => day === d.day) ?
-              plan.days.find(d => day === d.day)?.exercises.map((ex, i) => (
-                <div
-                  key={i}
-                  className="flex justify-between items-center py-2 border-b border-[#2e2e2e] last:border-0 text-[13px]"
-                >
-                  <span className="text-white">{ex.name}</span>
-                  <span className="text-(--wpg-muted-text-color)">
-                    {ex.sets} × {ex.reps}
-                  </span>
-                </div>
-              ))
-              : (
-                <div className="text-[#2e2e2e] text-[13px] tracking-[2px] uppercase text-center py-4">
-                  {day} - Rest
+        <div className="grid grid-cols-3 gap-4 mt-10">
+          {days.map((day, index) => (
+            <div
+              key={index}
+              className={`border p-5 ${
+                plan.days.find(d => day === d.day)
+                  ? "border-[#2e2e2e] bg-[#1a1a1a]"
+                  : "border-dashed border-[#2e2e2e] flex justify-center items-center "
+              }`}
+            >
+              {plan.days.find(d => day === d.day) ?
+              (
+                <div className="text-[22px] uppercase tracking-[1px] text-(--wpg-accent-color) mb-3">
+                  {day} — {plan.days.find(d => day === d.day)?.focus}
                 </div>
               )
-            }
-          </div>
-        ))}
+              : null
+              }
+
+              {plan.days.find(d => day === d.day) ?
+                plan.days.find(d => day === d.day)?.exercises.map((ex, i) => (
+                  <div
+                    key={i}
+                    className="flex justify-between items-center py-2 border-b border-[#2e2e2e] last:border-0 text-[13px]"
+                  >
+                    <span className="text-white">{ex.name}</span>
+                    <span className="text-(--wpg-muted-text-color)">
+                      {ex.sets} × {ex.reps}
+                    </span>
+                  </div>
+                ))
+                : (
+                  <div className="text-[#2e2e2e] text-[13px] tracking-[2px] uppercase text-center py-4">
+                    {day} - Rest
+                  </div>
+                )
+              }
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
