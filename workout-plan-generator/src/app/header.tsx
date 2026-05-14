4d7031aecb129483ed86f3d2368ba482926af451
Soheil Jamshidi <eris1376@gmail.com>
2026-05-14 20:38:42 +0200

    WPF [ADD] Updated the header for illustrating the image and modified the sign out button

--- workout-plan-generator/src/app/header.tsx
+++ workout-plan-generator/src/app/header.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { signOut, useSession } from "next-auth/react";
 import { use, useEffect, useState } from "react";
+import  userIcon  from "@/../public/default-user-icon.svg";
+
 
 type User = {
   name: string,
@@ -16,6 +18,7 @@ type Props = {}
 export default function Header(props: Props) {
   const { data: session, status} = useSession();
   const router = useRouter();
+  const [showDropDown, setShowDropDown] = useState<Boolean>(false);
 
   return(
     <>
@@ -44,13 +47,40 @@ export default function Header(props: Props) {
 
         <div className="flex items-center">
           {session ?
-            <button
-              className="bg-(--wpg-main-text-color) px-5 py-2 text-black text-[13px] hover:cursor-pointer"
-              type="button"
-              onClick={async () => await signOut()}
-            >
-              Sign out
-            </button>
+            <>
+              {/* <button
+                className="bg-(--wpg-main-text-color) px-5 py-2 text-black text-[13px] hover:cursor-pointer"
+                type="button"
+                onClick={async () => await signOut()}
+              >
+                Sign out
+              </button> */}
+
+              <div
+                className="relative w-10 h-10 border-2 border-(--wpg-main-text-color) rounded-full hover:cursor-pointer hover:opacity-80 transition-opacity"
+                onClick={() => setShowDropDown(!showDropDown)}
+              >
+                <img
+                  className="rounded-full"
+                  src={session.user?.image ? session.user?.image : userIcon.src}
+                  alt="User Icon"
+                />
+              </div>
+              {showDropDown ?
+                <div
+                  className="absolute top-17 right-8 min-w-36 bg-[#1a1a1a] border border-[#2e2e2e] p-2 rounded-md shadow-xl flex flex-col z-50 cursor-default"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <button
+                    className="w-full text-left bg-transparent px-4 py-2 text-(--wpg-main-text-color) hover:text-white hover:bg-[#2e2e2e] rounded-sm text-[13px] transition-colors cursor-pointer"
+                    type="button"
+                    onClick={async () => await signOut()}
+                  >
+                    Sign out
+                  </button>
+                </div>
+              : null}
+            </>
           :
             <button
               className="bg-(--wpg-main-text-color) px-5 py-2 text-black text-[13px] hover:cursor-pointer"
