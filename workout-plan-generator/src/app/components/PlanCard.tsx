203b3b8984bc59358ca5adc02e0ab5a4fe230d34
Soheil Jamshidi <eris1376@gmail.com>
2026-05-16 12:18:39 +0200

    WPG [ADD] Implemented PlanCard component

---
+++ workout-plan-generator/src/app/components/PlanCard.tsx
@@ -0,0 +1,39 @@
+"use client"
+import { redirect } from "next/navigation";
+import { SavedPlan } from "../lib/costumeTypes";
+
+export default function PlanCard({plan}: {plan: SavedPlan}) {
+  console.log(plan)
+  return(
+     <div
+        key={plan?.id}
+        className="flex flex-col justify-between border p-5 border-[#2e2e2e] bg-[#1a1a1a] hover:cursor-pointer"
+        onClick={() => {
+          redirect(`/plan/${plan.id}`)
+        }}
+      >
+        <div className="text-[22px] uppercase tracking-[1px] text-(--wpg-accent-color) mb-3"
+        >
+          {plan?.title}
+        </div>
+
+        <div className="flex flex-wrap gap-2">
+          <div className="tag">
+            {plan?.level}
+          </div>
+
+          <div className="tag">
+            {`${plan?.days} ${plan?.days === 1 ? "day" : "days"} / week`}
+          </div>
+
+          <div className="tag">
+            {`${plan?.time} min`}
+          </div>
+
+          <div className="tag">
+            {plan?.equipment}
+          </div>
+        </div>
+      </div>
+  )
+}
